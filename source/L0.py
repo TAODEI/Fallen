8240711caec9868893aaacb0516abcd64c3dd82f
DG_Gua <DGGua@foxmail.com>
2021-05-15 20:38:56 +0800

    给L0加了一些注释

--- source/L0.py
+++ source/L0.py
@@ -1,29 +1,36 @@
 import cv2
 import pygame
-from pygame.surface import Surface
+from pygame.surface import Surface, SurfaceType
 
 from source import setup
 
 
 class L0:
     def __init__(self):
+        # 是否进入下一层
         self.next = False
+        # 读取视频下一帧成功
         self.success = True
+        # 视频源
         self.video_resource = cv2.VideoCapture("resources/Fallen/movie.mp4")
-        self.count = 0
+        # 视频宽
         self.width = int(self.video_resource.get(cv2.CAP_PROP_FRAME_WIDTH))
+        # 视频高
         self.height = int(self.video_resource.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        # 循环计数
         self.time_count = 0
+        # 是否播放视频
         self.play_video = True
+        # 标题初始不透明度
         self.alpha = 0
+        # 标题不透明度每次增量
         self.add_alpha = 1
         self.title = setup.GRAPHICS['Title']
 
-    def video2frame(self):
-
+    def video2frame(self) -> [Surface, SurfaceType]:
+        """将视频读取，返回值为当前帧"""
         # 按帧读取视频
         self.success, image = self.video_resource.read()
-        self.count += 1
         if self.success:
             img = pygame.image.frombuffer(image.tostring(), (self.width, self.height), "RGB")
             self.success, image = self.video_resource.read()
@@ -34,6 +41,7 @@ class L0:
 
     def update(self, surface: Surface) -> object:
         self.time_count += 1
+        # 播放视频
         if self.play_video:
             if self.time_count % 7 == 0:
                 self.time_count = 0
@@ -41,8 +49,16 @@ class L0:
                 if frame is not None:
                     background = pygame.transform.scale(frame, (setup.WINDOW_WIDTH, setup.WINDOW_HEIGHT))
                     surface.blit(background, surface.get_rect())
+
+        # 出现标题
         elif self.time_count % 7 == 0:
+            self.time_count = 0
             self.title.set_alpha(self.alpha)
             self.alpha += self.add_alpha
             surface.blit(self.title, surface.get_rect())
-        return None
+
+        # 返回值判断（一个延时）
+        if self.alpha == 100 and self.time_count > 300:
+            return True
+        else:
+            return False
