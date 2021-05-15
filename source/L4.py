a9cf4b6bc4196ab24664d2620ad3f7f83d015746
TAODEI <864978550@qq.com>
2021-05-15 23:53:46 +0800

    1

--- source/L4.py
+++ source/L4.py
@@ -1,28 +1,9 @@
 from .import setup
 import pygame, time
 
-class Level2:
+class Level4:
     def __init__(self):
         self.ok = True
-        # 循环计数
-        self.time_count = 0
-        # 初始不透明度
-        self.alpha = 0
-        # 标题不透明度每次增量
-        self.add_alpha = 8
-        # 三个云
-        self.clouds = [setup.GRAPHICS['cloud.1'],
-                       setup.GRAPHICS['cloud.2'],
-                       setup.GRAPHICS['cloud.3']]
-        # 掉的人
-        self.girl = setup.GRAPHICS['girl']
-        # 背景
-        self.background = setup.GRAPHICS['3_1']
-        # 掉落人的高度
-        self.girl_height = -setup.WINDOW_HEIGHT + 200
-        # 掉落速度
-        self.fall_v = 15
-        #self.timer = pygame.
         self.state = 2
         self.background = setup.GRAPHICS['2_1']
         self.b2 = setup.GRAPHICS['2_2']
@@ -36,32 +17,7 @@ class Level2:
         self.current_time = pygame.time.get_ticks()
         self.timer = 0
 
-    def update_cloud_and_girl(self, surface: pygame.Surface):
-        self.time_count += 1
-
-        # 云和掉的人
-        if self.time_count % 7 == 0:
-            # 换云,掉人
-            self.girl_height += self.fall_v
-            cloud = self.clouds[int(self.time_count / 150) % 3]
-            cloud.set_alpha(self.alpha)
-            self.background.set_alpha(self.alpha * 2)
-            self.alpha += self.add_alpha
-            if self.alpha == 200:
-                self.add_alpha = 0
-            if self.ok:
-                surface.blit(self.background, surface.get_rect())
-            else:
-                surface.blit(self.b2, surface.get_rect())
-
-            surface.blit(self.girl, (0, self.girl_height, surface.get_rect().width,  surface.get_rect().height))
-            surface.blit(cloud, surface.get_rect())
-
-            if self.time_count > 500:
-                self.time_count = 0
-            if self.girl_height > setup.WINDOW_HEIGHT * 0.6:
-                self.ok = False
-    def update(self, surface, keys, dir):
+def update(self, surface, keys, dir):
         if self.state < 3:
             self.update_cloud_and_girl(surface)
         if 'down' in dir and self.ok == False:
