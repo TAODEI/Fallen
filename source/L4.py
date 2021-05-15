2a34b3c870f390d16775403a5ef3da8792f01923
TAODEI <864978550@qq.com>
2021-05-16 02:46:02 +0800

    1

--- source/L4.py
+++ source/L4.py
@@ -28,37 +28,50 @@ class Level4:
         self.cloud = pygame.sprite.Sprite()
         self.cloud.image = pygame.transform.scale(self.b2, (int(1536 / 2), int(2048 / 2)))
         rect = self.cloud.image.get_rect()
-        rect.x, rect.y = 0, 0
+        rect.x, rect.y = -120, -120
         self.cloud.rect = rect
-        self.move_x = 0
-        self.move_y = 0
         self.is_move = False
+        self.door = False
     def update_cloud(self, dir):
         w,h = self.cloud.image.get_size()
+        
+        #if is_in_rect(dir['x2'], dir['y2'], (self.cloud.rect.x, self.cloud.rect.y, w, h)):
+        self.cloud.rect.x = dir['x2'] - w/2 + 180
+        self.cloud.rect.y = dir['x2'] - w/2 + 180
+            #self.cloud.rect.y = dir['y2'] - h/2
         print(w, h, self.cloud.rect.x, self.cloud.rect.y)
-        if is_in_rect(dir['x'], dir['y'], (self.cloud.rect.x, self.cloud.rect.y, w, h)):
-            self.cloud.rect.x = dir['x'] - w/2
-            self.cloud.rect.y = dir['y'] - h/2
     def update(self, surface, keys, dir):
-        if 'down' in dir:
-            print(1111)
-            self.is_move = True
-        if 'up' in dir:
-            self.is_move = False
+        if self.door:
+            if 2000 > pygame.time.get_ticks() - self.timer:
+                surface.blit(self.b4, surface.get_rect())
+            if 4000 > pygame.time.get_ticks() - self.timer > 2000:
+                surface.blit(self.b5, surface.get_rect())
+        elif self.cloud.rect.x > 305:
+            if 'down' in dir:
+                print(dir['x'], dir['y'])
+                if 260 > dir['x'] > 216 and 272 > dir['y'] > 251:
+                    self.door = True
+                    self.timer = pygame.time.get_ticks()
+        else:
+            if 'down' in dir:
+                self.is_move = True
+            if 'up' in dir:
+                self.is_move = False
 
-        if self.state == -1:
-            self.timer = pygame.time.get_ticks()
-            self.state = 0
-        if pygame.time.get_ticks() - self.timer < 1000:
-            surface.blit(self.background, surface.get_rect())
-        if 1000 < pygame.time.get_ticks() - self.timer < 2000:
-            surface.blit(self.b2, surface.get_rect())
-        if pygame.time.get_ticks() - self.timer > 2000:
+            if self.state == -1:
+                self.timer = pygame.time.get_ticks()
+                self.state = 0
+            if pygame.time.get_ticks() - self.timer < 1000:
+                surface.blit(self.background, surface.get_rect())
+            if 1000 < pygame.time.get_ticks() - self.timer < 2000:
+                surface.blit(self.b2, surface.get_rect())
+            if pygame.time.get_ticks() - self.timer > 2000:
 
-            surface.blit(self.b3, surface.get_rect())
-            surface.blit(self.cloud.image, self.cloud.rect)
-            if self.is_move:
-                self.update_cloud(dir)
+                surface.blit(self.b3, surface.get_rect())
+                surface.blit(self.cloud.image, self.cloud.rect)
+                if self.is_move:
+                    self.update_cloud(dir)
+        
             #if self.state < 3:
                 #self.update_cloud_and_girl(surface)
             '''
