e77ff5899576c20cc6bc10f96d68d0029209ee9e
DG_Gua <DGGua@foxmail.com>
2021-05-16 00:01:53 +0800

    L3 的自然的云动画做好了

--- source/L3.py
+++ source/L3.py
@@ -16,7 +16,7 @@ class L3:
         # 初始不透明度
         self.alpha = 0
         # 标题不透明度每次增量
-        self.add_alpha = 8
+        self.add_alpha = 15
         # 三个云
         self.clouds = [setup.GRAPHICS['cloud.1'],
                        setup.GRAPHICS['cloud.2'],
@@ -29,23 +29,83 @@ class L3:
         self.girl_height = -setup.WINDOW_HEIGHT
         # 掉落速度
         self.fall_v = 15
+        # 各阶段结束控制
+        self.isOK = [False, False,False,False]
+        # 云的不透明度
+        self.cloud_index = 0
+        self.cloud_alpha = 255
+        self.add_cloud_alpha = -8
 
     def update(self, surface: Surface, keys, dir) -> object:
         self.time_count += 1
-
         # 云和掉的人
-        if self.time_count % 7 == 0:
-            # 换云,掉人
-            self.girl_height += self.fall_v
-            cloud = self.clouds[int(self.time_count / 150) % 3]
-            cloud.set_alpha(self.alpha)
-            self.background.set_alpha(self.alpha * 2)
-            self.alpha += self.add_alpha
-            if self.alpha == 200:
-                self.add_alpha = 0
-            surface.blit(self.background, surface.get_rect())
-            surface.blit(self.girl, (0, self.girl_height, surface.get_rect().width,  surface.get_rect().height))
-            surface.blit(cloud, surface.get_rect())
+        if not self.isOK[0]:
+            if self.time_count % 7 == 0:
 
-            if self.time_count > 500:
+                self.girl_height += self.fall_v
+
+                self.cloud_alpha += self.add_cloud_alpha
+                cloud_now = self.clouds[self.cloud_index]
+                cloud_next = self.clouds[(self.cloud_index+1)%3]
+                cloud_now.set_alpha(int(self.alpha * self.cloud_alpha/255))
+                cloud_next.set_alpha(int(self.alpha * (1-self.cloud_alpha/255)))
+
+                self.background.set_alpha(self.alpha * 2)
+                self.alpha += self.add_alpha
+                if self.alpha > 255:
+                    self.add_alpha = 0
+
+                surface.blit(self.background, surface.get_rect())
+                surface.blit(self.girl, (0, self.girl_height, surface.get_rect().width, surface.get_rect().height))
+                surface.blit(cloud_next, surface.get_rect())
+                surface.blit(cloud_now, surface.get_rect())
+
+                if self.girl_height > setup.WINDOW_HEIGHT :
+                    self.isOK[0] = True
+                    self.time_count = 0
+                if self.time_count > 1000:
+                    self.time_count = 0
+                if self.cloud_alpha < 0 :
+                    self.cloud_alpha =255
+                    self.cloud_index = (self.cloud_index+1)%3
+
+        # 缩放图像到指定位置
+        elif not self.isOK[1]:
+            self.time_count += 1
+            if self.time_count % 7 == 0:
+
+                self.background = setup.GRAPHICS['3_2']
+                cloud_now = self.clouds[self.cloud_index]
+                cloud_next = self.clouds[(self.cloud_index + 1) % 3]
+                cloud_now.set_alpha(int(self.alpha * self.cloud_alpha / 255))
+                cloud_next.set_alpha(int(self.alpha * (1 - self.cloud_alpha / 255)))
+
+                surface.blit(self.background, surface.get_rect())
+                surface.blit(cloud_next, surface.get_rect())
+                surface.blit(cloud_now, surface.get_rect())
+
+                if self.cloud_alpha < 0:
+                    self.cloud_alpha = 255
+                    self.cloud_index = (self.cloud_index + 1) % 3
+                self.cloud_alpha += self.add_cloud_alpha
+                if self.time_count > 500:
+                    self.time_count = 0
+                    self.isOK[1] = True
+        # 变为指定图像
+        elif not self.isOK[2]:
+            if self.time_count > 700:
                 self.time_count = 0
+                self.isOK[2] = True
+                self.background = setup.GRAPHICS['3_3']
+                surface.blit(self.background, surface.get_rect())
+        # 变为好几个人
+        elif not self.isOK[3]:
+            if self.time_count > 300:
+                self.time_count = 0
+                self.isOK[3] = True
+                self.background = setup.GRAPHICS['3_4']
+                surface.blit(self.background, surface.get_rect())
+        elif not self.isOK[4]:
+            pass
+
+        return None
