f4eaeebef9458c5f3761ee5b05e0b4e055f8fe37
DG_Gua <DGGua@foxmail.com>
2021-05-16 08:04:22 +0800

    好几层好了

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from source.Game_Engine import GameEngine
 from source import L2
 #from source.L2 import Level2
 from source.L0 import L0
+from source.L11 import L11
 from source.L3 import L3
 from source.L5 import L5
 from source.L6 import L6
+from source.L9 import L9
 
-floors = [L6()]
+floors = [L9()]
 
 
 def main():
